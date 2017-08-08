d679e2b056850a24cd3fc552210723c5dea870cb
Mohmed Bamhraz <mohmedbamhrz@gmail.com>
2017-08-08 10:57:23 -0700

    - add questions deatils

--- index.js
+++ index.js
@@ -43,3 +43,71 @@ const Question = {
 // usage:
 Question.getAll()
 // Question.get(344)
+
+// NOTE: Node Selector Heplers
+function q (query) { return document.querySelector(query); }
+function qs (query) { return document.querySelectorAll(query); }
+// NOTE: View
+
+function renderQuestions (questions = []) {
+  return questions
+    .map(question => `
+      <div class='question-summary'>
+        <a data-id = ${question.id} href>${question.title}</a>
+      </div>
+    `)
+    .join('');
+}
+// NOTE: this renderQuestion function will render each question when we click on the title from the show page
+function renderQuestion (question = {}) {
+  const {author = {}} = question;
+  return `
+    <h1>${question.title}</h1>
+    <p>${question.body}</p>
+    <p>By <strong>${author.first_name} ${author.last_name}</strong></p>
+    <button></button>
+  `
+}
+
+document.addEventListener('DOMContentLoaded', event => {
+  // NOTE:  write code that needs to run after the DOM is fully loaded in here
+  const questionList = q('#question-list');
+  const questionDetails = q('#question-details')
+
+  // NOTE: render a single question
+  // Question
+  //   .get(331)
+  //   .then(renderQuestion)
+  //   .then(html => {
+  //     questionDetails.innerHTML = html;
+  //   })
+
+  // NOTE: render all questions
+  Question
+    .getAll()
+    .then(renderQuestions)
+    .then(html => {
+      questionList.innerHTML = html;
+    });
+
+    questionList.addEventListener('click', event => {
+      const {target} = event;
+
+      if(target.matches('.question-summary > a')){
+        event.preventDefault();
+        const id = target.getAttribute('data-id');
+
+        Question
+          .get(id)
+          .then(renderQuestion)
+          .then(html => {
+            questionDetails.innerHTML = html
+            questionDetails.classList.remove('hidden');
+            questionList.classList.add('hidden')
+          })
+
+
+        // console.log(target.getAttribute(`data-id`));
+      }
+    });
+});
